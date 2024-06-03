b4414c60f25146bb45c527a6a5fd07bdeaba7366
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-03 15:32:51 +0300

    display images of cars with title

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+import Car1 from "@/assets/Car1.jpeg";
+import Car2 from "@/assets/Car2.jpeg";
+import Car3 from "@/assets/Car3.jpeg";
+import Car4 from "@/assets/Car4.jpeg";
+
 export default function Car() {
   const headerListItems = [
     "Build you kit",
@@ -7,6 +13,7 @@ export default function Car() {
   ];
   return (
     <>
+      {/* Header */}
       <div className="headerContainer col-lg-12">
         <div className="headerSubContainer col-lg-11 col-xl-7">
           <div className="logoContainer col-lg-3">BRANDNAME</div>
@@ -21,6 +28,46 @@ export default function Car() {
             <button className="signinbtn">Sign in</button>
             <button className="btn btn-primary">Sign up</button>
           </div>
+        </div>
+      </div>
+      {/* Content */}
+      <div className="contentContainer col-lg-11 col-xl-7">
+        <div className="shopFilterPanel col-lg-4">hi</div>
+        <div className="col-lg-8">
+          <div className="shopCards">
+            <div className="col-lg-6">
+              <Image className="shopImage" src={Car1} alt="Shop Image"></Image>
+            </div>
+            <div className="shopDescriptionHolder col-lg-6">
+              <h6 className="white-text">Volkswagen Golf R 2.0 TSI R 5dr</h6>
+            </div>
+          </div>
+          <div className="shopCards">
+            <div className="col-lg-6">
+              <Image className="shopImage" src={Car2} alt="Shop Image"></Image>
+            </div>
+            <div className="shopDescriptionHolder col-lg-6">
+              <h6 className="white-text">Volkswagen Golf R 2.0 TSI R 5dr</h6>
+            </div>
+          </div>
+          <div className="shopCards">
+            <div className="col-lg-6">
+              <Image className="shopImage" src={Car3} alt="Shop Image"></Image>
+            </div>
+            <div className="shopDescriptionHolder col-lg-6">
+              <h6 className="white-text">Volkswagen Golf R 2.0 TSI R 5dr</h6>
+            </div>
+          </div>
+          <div className="shopCards">
+            <div className="col-lg-6">
+              <Image className="shopImage" src={Car4} alt="Shop Image"></Image>
+            </div>
+            <div className="shopDescriptionHolder col-lg-6">
+              <h6 className="white-text">Volkswagen Golf R 2.0 TSI R 5dr</h6>
+            </div>
+          </div>
+          <div className="shopCards">Hello</div>
+          <div className="shopCards">Hello</div>
         </div>
       </div>
     </>
