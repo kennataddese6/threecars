7dc38b41090816e229d2cb4a0d06305469badf1a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-03 17:02:40 +0300

    add content to the the car detail

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -3,6 +3,10 @@ import Car1 from "@/assets/Car1.jpeg";
 import Car2 from "@/assets/Car2.jpeg";
 import Car3 from "@/assets/Car3.jpeg";
 import Car4 from "@/assets/Car4.jpeg";
+import { FaGear, FaGasPump } from "react-icons/fa6";
+import { FaTachometerAlt } from "react-icons/fa";
+import { MdDateRange } from "react-icons/md";
+import { IoIosColorPalette } from "react-icons/io";
 export default function Car() {
   const headerListItems = [
     "Build you kit",
@@ -14,18 +18,42 @@ export default function Car() {
     {
       name: "Volkswagen Golf R 2.0 TSI R 5dr",
       image: Car1,
+      gear: "Automatic",
+      date: 2019,
+      gas: "Petrol",
+      color: "Blue",
+      miles: "240 Km/h",
+      trending: true,
     },
     {
       name: "Volkswagen Golf R 2.0 TSI R 5dr",
       image: Car2,
+      gear: "Manual",
+      date: 2023,
+      gas: "Diesel",
+      color: "Red",
+      miles: "320 Km/h",
+      trending: false,
     },
     {
       name: "Volkswagen Golf R 2.0 TSI R 5dr",
       image: Car3,
+      gear: "Semiauto",
+      date: 2024,
+      gas: "Gasoline",
+      color: "Grey",
+      miles: "180 Km/h",
+      trending: true,
     },
     {
       name: "Volkswagen Golf R 2.0 TSI R 5dr",
       image: Car4,
+      gear: "Automatic",
+      date: 2024,
+      gas: "Petrol",
+      color: "Red",
+      miles: "340 Km/h",
+      trending: false,
     },
   ];
   return (
@@ -53,15 +81,39 @@ export default function Car() {
         <div className="col-lg-8">
           {carList.map((car) => (
             <div className="shopCards">
-              <div className="col-lg-6">
+              <div className="col-lg-5">
+                {car.trending && <div className="tredingBanner">Trending </div>}
                 <Image
                   className="shopImage"
                   src={car.image}
                   alt={car.name}
                 ></Image>
               </div>
-              <div className="shopDescriptionHolder col-lg-6">
+              <div className="shopDescriptionHolder col-lg-7">
                 <h6 className="white-text">{car.name}</h6>
+                <div className="moredetailContainer">
+                  <div className="d-flex col-lg-6">
+                    <FaGear color="white" fontSize={22} />
+                    <p className="px-2 white-text">{car.gear}</p>
+                  </div>
+                  <div className="d-flex col-lg-6">
+                    <FaGasPump color="white" fontSize={22} />
+                    <p className="px-2 white-text">{car.gas}</p>
+                  </div>
+                  <div className="d-flex col-lg-6">
+                    <MdDateRange color="white" fontSize={22} />
+                    <p className="px-2 white-text">{car.date}</p>
+                  </div>
+                  <div className="d-flex col-lg-6">
+                    <IoIosColorPalette color="white" fontSize={22} />
+                    <p className="px-2 white-text">{car.color}</p>
+                  </div>
+                  <div className="d-flex col-lg-6">
+                    <FaTachometerAlt color="white" fontSize={22} />
+                    <p className="px-2 white-text">{car.miles}</p>
+                  </div>
+                </div>
+                <button className="buildbutton btn btn-primary">Build</button>
               </div>
             </div>
           ))}
