b7ee80da1ab0d745ab4d283a2119625ce94b263a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-22 17:47:29 +0300

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -531,8 +531,16 @@ export default function Home() {
         </div>
       </div>
       <div className="copyRightFooter">
-        <h6 className="white-text">MT Lights</h6>
-        <h6 className="white-text">Instagram Email</h6>
+        <footer className="col-lg-11 col-xl-9">
+          <div>
+            <h6 className="white-text logoContainer">MT Lights</h6>
+            <MdLocationOn fontSize={32} color="white" />
+            <div>
+              <p>25566 Hc 1, Glenallen, Alaska, 99588, USA</p>
+            </div>
+          </div>
+          <h6 className="white-text">Instagram Email</h6>
+        </footer>
       </div>
     </>
   );
