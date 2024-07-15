c48b836289fc1ee70b71489352c947d1835ae9e1
unknown <kennataddese6@gmail.com>
2024-07-15 13:04:31 -0700

    make footer responsive

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -6,51 +6,36 @@ import { FaXTwitter } from 'react-icons/fa6';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <div className="copyRightFooter">
-      <footer className="col-lg-11 col-xl-10">
-        <div className="footerItem">
-          <h5 className="white-text p-3">MT Custom Lights</h5>
-          <p className="white-text p-3">
-            <MdLocationOn color="white" fontSize={32} />
-            <span className="ms-1">California, USA</span>
-          </p>
-          <p className="white-text p-3">
-            <MdPhone color="white" fontSize={32} />
-            <span className="ms-1">+251 991 374186</span>
-          </p>
-          <p className="white-text p-3">
-            <MdOutlineEmail color="white" fontSize={32} />
-            <span className="ms-2">mtcustomlights@gmail.com</span>
-          </p>
-        </div>
-        <div className="footerItem">
-          <h5 className="white-text p-3">Our Product</h5>
-          <p className="white-text p-3">Career</p>
-          <p className="white-text p-3">Lights</p>
-          <p className="white-text p-3">Kits</p>
-        </div>
-        <div className="footerItem">
-          <h5 className="white-text p-3">Follow us</h5>
-          <p className="white-text p-3">
-            <CiFacebook color="white" fontSize={32} />
-            <Link href="#" className="ms-1">
-              https://facebook.com/mtlithts
-            </Link>
-          </p>
-          <p className="white-text p-3">
-            <CiInstagram color="white" fontSize={32} />
-            <Link href="#" className="ms-1">
-              https://facebook.com/mtlithts
-            </Link>
-          </p>
-          <p className="white-text p-3">
-            <FaXTwitter color="white" fontSize={32} />
-            <Link href="#" className="ms-1">
-              https://facebook.com/mtlithts
-            </Link>
-          </p>
-        </div>
-      </footer>
-    </div>
+    <footer className="col-lg-11 col-xl-10">
+      <div className="footerItem">
+        <h5 className="white-text p-3">MT Custom Lights</h5>
+        <p className="white-text p-3">
+          <MdLocationOn color="white" fontSize={32} />
+          <span className="ms-1">California, USA</span>
+        </p>
+        <p className="white-text p-3">
+          <MdPhone color="white" fontSize={32} />
+          <span className="ms-1">+251 991 374186</span>
+        </p>
+        <p className="white-text p-3">
+          <MdOutlineEmail color="white" fontSize={32} />
+          <span className="ms-2">mtcuss@gmail.com</span>
+        </p>
+      </div>
+      <div className="footerItem">
+        <h5 className="white-text p-3">Our Product</h5>
+        <p className="white-text p-3">Build kit</p>
+        <p className="white-text p-3">Parts for sale</p>
+        <p className="white-text p-3">Work with us</p>
+      </div>
+      <div className="footerItem">
+        <h5 className="white-text p-3">Follow us</h5>
+        <p className="white-text p-3">
+          <CiFacebook color="white" fontSize={32} className="m-1" />
+          <FaXTwitter color="white" fontSize={32} className="m-1" />
+          <CiInstagram color="white" fontSize={32} />
+        </p>
+      </div>
+    </footer>
   );
 }
